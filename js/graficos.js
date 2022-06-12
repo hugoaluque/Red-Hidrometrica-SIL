2af3626404ba788e4270a386865f755d1976e50e
Hugo Añamuro <hugo.aluque@gmail.com>
2022-06-12 01:42:15 -0500

    INICIO

---
+++ js/graficos.js
@@ -0,0 +1,336 @@
+datosGeojson = {
+	"type": "FeatureCollection",
+	"name": "graficos",
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 1,
+				"Nombre": "PARQUE NATURAL DE PUCALLPA",
+				"Categoria": "AREA NATURAL PROTEGIDA"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.55977847776167,
+					-8.389928460941205
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 2,
+				"Nombre": "REAL PLAZA",
+				"Categoria": "SUPERMERCADO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.556862707181,
+					-8.384609734150077
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 3,
+				"Nombre": "COLEGIO INDUSTRIAL ALFREDO VARGAS GUERRA",
+				"Categoria": "EDUCACION"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.568061,
+					-8.38766
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 4,
+				"Nombre": "SENATI",
+				"Categoria": "EDUCACION"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.564928,
+					-8.387097
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 5,
+				"Nombre": "OPEN PLAZA",
+				"Categoria": "SUPERMERCADO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.566709,
+					-8.387331
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 6,
+				"Nombre": "HOSPEDAJE MARVENTO",
+				"Categoria": "HOSPEDAJE"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.5632871147204,
+					-8.38664563363487
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 7,
+				"Nombre": "AEROPUERTO INTERNACIONAL CAPITAN FAP DAVID ABINZUR RENGIFO",
+				"Categoria": "AEROPUERTO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.57765524729106,
+					-8.386586727155294
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 8,
+				"Nombre": "SUNAT",
+				"Categoria": "OFICINAS ADMINISTRATIVAS"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.57299929354737,
+					-8.388276421720944
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 9,
+				"Nombre": "INSTITUCION EDUCATIVA SUDAMERIZ",
+				"Categoria": "EDUCACION"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.56702857480232,
+					-8.384597705078433
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 10,
+				"Nombre": "HOSPEDAJE CRISTOFER",
+				"Categoria": "HOSPEDAJE"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.56464003679479,
+					-8.38951460350977
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 11,
+				"Nombre": "MERCADO MICAELA BASTIDAS",
+				"Categoria": "MERCADO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.56164940631594,
+					-8.383983583535456
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 12,
+				"Nombre": "GRIFO EL ORIENTE",
+				"Categoria": "GRIFO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.56541138985966,
+					-8.38491811838914
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 13,
+				"Nombre": "IGLESIA DE COREA",
+				"Categoria": "IGLESIA"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.55976493381066,
+					-8.384903412259427
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 14,
+				"Nombre": "INSTITUCION EDUCATIVA INICIAL HOSANNA",
+				"Categoria": "EDUCACION"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.55953722557838,
+					-8.383519467248876
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 15,
+				"Nombre": "GRIFO ESPINOZA",
+				"Categoria": "GRIFO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.557418,
+					-8.385984
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 16,
+				"Nombre": "CEMENTERIO DE PUCALLPA",
+				"Categoria": "CEMENTERIO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.570882,
+					-8.392118
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 17,
+				"Nombre": "INSTITUTO SUPERIOR TECNOLOGICO SUIZA",
+				"Categoria": "EDUCACION"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.575796,
+					-8.393009
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 18,
+				"Nombre": "UNIVERSIDAD NACIONAL DE UCAYALI",
+				"Categoria": "EDUCACION"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.5775444133107,
+					-8.393098405951753
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 19,
+				"Nombre": "GRIFO TO & CHEL",
+				"Categoria": "GRIFO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.569225,
+					-8.382906
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 20,
+				"Nombre": "INSTITUTO SUPERIOR PEDAGOGICO DE PUCALLPA",
+				"Categoria": "EDUCACION"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.568672,
+					-8.385464
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 21,
+				"Nombre": "MONUMENTO EL CAMPESINO",
+				"Categoria": "MONUMENTO"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.571097,
+					-8.389789
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 22,
+				"Nombre": "CENTRO DE SALUD FRATERNIDAD",
+				"Categoria": "SALUD"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-74.56496678913953,
+					-8.393809481602619
+				]
+			}
+		}
+	]
+}
